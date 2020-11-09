5cf35df5482fef31fb742cbcb01c248aded26005
cameron <cameron.leverett@yahoo.com>
2020-11-09 15:20:37 -0800

    uncommeted something I need for testing

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,8 +10,7 @@ const Profile = () => {
       <img src={user.picture} alt={user.name} />
       <h2>{user.name}</h2>
       <p>{user.email}</p>
-      {/* <JSONPretty data={user} /> */}
-      {/* { JSON.stringify(user, null, 2) } */}
+      <JSONPretty data={user} />
     </div>
   )
 }
