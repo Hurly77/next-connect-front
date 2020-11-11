cca0da7b4720e1e65edd35b5de28aa74801e31d7
cameron <cameron.leverett@yahoo.com>
2020-11-10 16:12:24 -0800

    creating store and middler ware

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react'
 
+import { Auth0Provider } from '@auth0/auth0-react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(applyMiddleware(thunk))
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
-   <Auth0Provider
+  <Provider store={store}>
+    <Auth0Provider
    domain={domain}
    clientId={clientId}
    redirectUri={window.location.origin}>
      <App />
-  </Auth0Provider>,
+  </Auth0Provider>
+  </Provider>,
   document.getElementById('root')
 );
 
