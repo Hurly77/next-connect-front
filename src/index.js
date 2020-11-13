12ed293e592fb287ad042545ed1f98437fc8d0a0
cameron <cameron.leverett@yahoo.com>
2020-11-12 17:22:44 -0800

    moved extra stuff into seperate space

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-import { Auth0Provider } from '@auth0/auth0-react';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 
-const store = createStore(applyMiddleware(thunk),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+import { store } from './_helpers/store';
+import { App } from './App/App';
 
-ReactDOM.render(
-  <Auth0Provider
-   domain={domain}
-   clientId={clientId}
-   redirectUri={window.location.origin}>
-  <Provider store={store}> 
-  </Provider>
-     <App />
-  </Auth0Provider>,
-  document.getElementById('root')
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
 );
-
-
