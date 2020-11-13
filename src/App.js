8c9f0bb61164fd147047140d53d9d01e032254da
cameron <cameron.leverett@yahoo.com>
2020-11-13 15:58:02 -0800

    moved file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import RegisterContainer from './containers/RegisterContainer';
+
+export default class App extends React.Component {
+
+	render() {
+		return (
+		 <div>
+			 <RegisterContainer />
+		 </div>
+		);
+	}
+}
