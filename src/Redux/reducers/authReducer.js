2c5c487584f80aa7a49968b13f01b5f309debc98
cameron <cameron.leverett@yahoo.com>
2020-11-16 17:22:32 -0800

    added a logout reducer for store

--- src/Redux/reducers/authReducer.js
+++ src/Redux/reducers/authReducer.js
@@ -1,14 +1,22 @@
 
-const authReducer = (state = {user: {}, loading: false}, action) => {
+const authReducer = (state = { loggedIn: false, currentUser: {}}, action) => {
   console.log(action)
   switch(action.type) {
     case 'AUTH_SUCCESS':
-      return {...state, user: action.user, loading: false}
-
-      default:
-        return state;
+     return {
+       ...state,
+       loggedIn: action.payload.loggedIn,
+       currentUser: action.payload.currentUser
+     }
+     case 'LOGOUT':
+      return {
+        ...state,
+        loggedIn: false,
+        currentUser: {},
+      };
+    default:
+      return state;
   }
-
 }
 
 export default authReducer;
