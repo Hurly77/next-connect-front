547bfd3b353b65e458cc3de8336ff3e0bf4737c9
cameron <cameron.leverett@yahoo.com>
2020-11-16 17:19:49 -0800

    working on app navagation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,9 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar-nav">
-      <NavLink to="/" exact>Feed</NavLink>      
-      <NavLink to="Inbox" exact>Inbox</NavLink>     
+      <NavLink to="/feed" exact>Feed</NavLink>      
+      <NavLink to="/inbox" exact>Inbox</NavLink>     
+      <NavLink to="/profile" exact>Inbox</NavLink>     
     </div>
   )
 }
