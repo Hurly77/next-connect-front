6bd044992d27e66713bbdcab4b250491d171d225
cameron <cameron.leverett@yahoo.com>
2020-11-16 17:19:24 -0800

    started feed component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Feed = () => {
+const Feed = ({user}) => {
+  console.log(user)
   return (
     <div>
       <h1>This is You Feed</h1>
+      <div className="container">
+        <h1>{user.first_name}</h1>
+        <p>{user.email}</p>
+      </div>
     </div>
   )
 }
 
-export default Feed
+const mapStateToProps = state => {
+  return {
+    user: state.auth.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Feed)
