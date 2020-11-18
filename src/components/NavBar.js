0a2237d8151c1ea188358c32096c890f792c8961
cameron <cameron.leverett@yahoo.com>
2020-11-17 19:09:45 -0800

    only if login can they view navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { checkLoggedIn, logout } from '../Redux/actions/authActions'
+import { Link } from 'react-router-dom'
 
-const NavBar = () => {
-  return (
-    <div className="navbar-nav">
-      <NavLink to="/feed" exact>Feed</NavLink>      
-      <NavLink to="/inbox" exact>Inbox</NavLink>     
-      <NavLink to="/profile" exact>Inbox</NavLink>     
-    </div>
-  )
+const NavBar = ({loggedIn}) => {
+  if(loggedIn) {
+    return (
+      <div className="navbar-nav">      
+        <NavLink to="/feed" exact>Feed</NavLink>      
+        <NavLink to="/inbox" exact>Inbox</NavLink>     
+        <NavLink to="/profile" exact>Your Profile</NavLink>
+        <Link to="#" onClick={() => this.props.logout(this.props.history)}>
+              Logout
+            </Link>
+      </div>
+    )
+  } else {
+   return ( <div>
+
+    </div>)
+  }
+  
+}
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.auth.loggedIn
+  }
 }
 
-export default NavBar
+const mapDispatchToProps = dispatch => {
+  return {
+    checkLoggedIn: () => dispatch(checkLoggedIn()),
+    logout: () => dispatch(logout())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
