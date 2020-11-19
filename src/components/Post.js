dfbe6d9cf1b8e1508d6cbf2ce3758e536c5f3d0f
cameron <cameron.leverett@yahoo.com>
2020-11-18 17:34:48 -0800

    adding new component for posts

---
+++ src/components/Post.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <h1>Hello</h1>  
+        </div>        
+        <div className="container">
+          <h1>Hello</h1>  
+        </div>        
+        <div className="container">
+          <h1>Hello</h1>  
+        </div>        
+        <div className="container">
+          <h1>Hello</h1>  
+        </div>        
+      </div>
+    )
+  }
+}
+
+export default Post
