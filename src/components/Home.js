74da8bce8b3a1afb7ab6964149f471fc1f3c823d
cameron <cameron.leverett@yahoo.com>
2020-11-18 17:33:57 -0800

    User won't see if logged in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+import { checkLoggedIn } from '../Redux/actions/authActions'
+import { Link } from 'react-router-dom'
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <h1>THIS IS HOME!!</h1>
-      </div>
-    )
+const Home = ({loggedIn}) => {
+    if(!loggedIn){
+      return (
+        <div>
+          <h1>Welocome to, Next-connect</h1>
+          <Link to="/login">Login</Link>
+          <br />
+          <Link to="/signup">Signup</Link>
+        </div>
+      )
+    } else {
+     return( <>
+        <h1>Welcome</h1>
+      </>)
+    }
+}
+
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.auth.loggedIn
   }
 }
+
+export default connect(mapStateToProps, {checkLoggedIn})(Home)
