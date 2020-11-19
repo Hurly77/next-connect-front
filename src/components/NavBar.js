54754e72b4d423f58745b0a77a278b5873470711
cameron <cameron.leverett@yahoo.com>
2020-11-18 17:34:30 -0800

    add withRouter component, to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,39 +1,40 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { checkLoggedIn, logout } from '../Redux/actions/authActions'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import {connect} from 'react-redux';
+import {NavLink} from 'react-router-dom';
+import {logout} from '../Redux/actions/authActions';
+import {withRouter} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const NavBar = ({loggedIn}) => {
-  if(loggedIn) {
-    return (
-      <div className="navbar-nav">      
-        <NavLink to="/feed" exact>Feed</NavLink>      
-        <NavLink to="/inbox" exact>Inbox</NavLink>     
-        <NavLink to="/profile" exact>Your Profile</NavLink>
-        <Link to="#" onClick={() => this.props.logout(this.props.history)}>
-              Logout
-            </Link>
-      </div>
-    )
-  } else {
-   return ( <div>
-
-    </div>)
-  }
-  
-}
-const mapStateToProps = state => {
-  return {
-    loggedIn: state.auth.loggedIn
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    checkLoggedIn: () => dispatch(checkLoggedIn()),
-    logout: () => dispatch(logout())
-  }
+		if (loggedIn) {
+			return (
+				<div className="navbar-nav">
+					<NavLink to="/feed" exact>
+						Feed
+					</NavLink>
+					<NavLink to="/inbox" exact>
+						Inbox
+					</NavLink>
+					<NavLink to="/profile" exact>
+						Your Profile
+					</NavLink>
+					<Link to="#" onClick={() => this.props.logout(this.props.history)}>
+						Logout
+					</Link>
+				</div>
+			);
+		} else {
+			return (
+				<>
+				</>
+			)
+		}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
+const mapStateToProps = (state) => {
+	return {
+		loggedIn : state.auth.loggedIn,
+	};
+};
+
+export default withRouter(connect(mapStateToProps, {logout})(NavBar));
