5f2dcf729bc2656bd962e443f3bfbfd3f9a4efe7
cameron <cameron.leverett@yahoo.com>
2020-11-20 18:36:27 -0800

    moved

---
+++ src/components/NavBar/Logout.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link, withRouter } from 'react-router-dom'
+import { logout } from '../../Redux/actions/authActions'
+
+class Logout extends Component {
+  render () {
+    if (this.props.loggedIn){
+      return (
+        <div className="logout-div">
+         <Link to="#" onClick={() => this.props.logout(this.props.history)}>
+              <button type="button" className="btn btn-primary logout">Logout</button>
+          </Link>
+        </div>
+      )
+      }else{
+       return( <>
+        </>)
+      }
+  }
+}
+const mapStateToProps = (state) => {
+	return {
+		loggedIn : state.auth.loggedIn,
+	};
+};
+
+export default withRouter(connect(mapStateToProps, {logout})(Logout))
