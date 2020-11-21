86e01a9e3715db159a20761b4d808da7fd612273
cameron <cameron.leverett@yahoo.com>
2020-11-21 13:19:35 -0800

    added Post to Post comonents

---
+++ src/containers/Post.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Comment from '../components/Feed/Comment'
+
+export default class Post extends Component {
+  render() {
+    return (
+      <div>
+        <Comment />
+      </div>
+    )
+  }
+}
