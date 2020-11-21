5e4549052369e63c65d4e822a850494532b40798
cameron <cameron.leverett@yahoo.com>
2020-11-21 13:20:15 -0800

    added FeedPage to pages

---
+++ src/Pages/FeedPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Post from '../containers/Post'
+
+export default class FeedPage extends Component {
+  render() {
+    return (
+      <div>
+        <Post />
+      </div>
+    )
+  }
+}
