023012539dd22291c7dd15babc244dc412705108
cameron <cameron.leverett@yahoo.com>
2020-11-21 16:34:26 -0800

    M-IU, to complicated to learn with such lil time

--- src/components/NavBar/PrimarySearchBar.js
+++ src/components/NavBar/PrimarySearchBar.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import TextField from '@material-ui/core/TextField'
 
 class PrimarySearchBar extends Component {
 
   render() {
     return (
-      <div className="primarySearch">
-          <TextField label="Search" />
+      <div>
+          <input type="text" name="search" placeholder="Search" className="search" />
       </div>
     )
   }
