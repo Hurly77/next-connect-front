dd968ab51b77b211bf65bc8345bbb97755ce7ffa
cameron <cameron.leverett@yahoo.com>
2020-11-21 16:35:12 -0800

    componet for already posted by user or connections

---
+++ src/containers/Posted.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Post from '../components/Feed/Post'
+
+export default class Posted extends Component {
+  render() {
+    return (
+      <div className="posted">
+        <Post />
+      </div>
+    )
+  }
+}
