71b54dd39b90279acf068851176a4181b2581bb5
cameron <cameron.leverett@yahoo.com>
2020-11-21 16:32:38 -0800

    Component for adding new post.

---
+++ src/components/Feed/NewPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class NewPost extends Component {
+  state = {
+    text: ''
+  }
+
+  render() {
+    return (
+      <input></input>
+    )
+  }
+}
