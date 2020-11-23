77d8694d19a9d18ffb94c47cc885e2dec4d92d63
cameron <cameron.leverett@yahoo.com>
2020-11-22 20:52:59 -0800

    no longer need to pass in prop

--- src/containers/SiteBar.js
+++ src/containers/SiteBar.js
@@ -4,6 +4,7 @@ import ToolBar from '@material-ui/core/Toolbar'
 import NavMenu from '../components/NavBar/NavMenu'
 import PrimarySearchBar from '../components/NavBar/PrimarySearchBar'
 import { makeStyles } from '@material-ui/core/styles'
+import { connect } from 'react-redux'
 
 const useStyles = makeStyles({
   root: {
@@ -12,17 +13,23 @@ const useStyles = makeStyles({
   }
 })
 
-const SiteBar = () => {
+const SiteBar = ({loggedIn}) => {
 
     const classes = useStyles()
     return (
       <AppBar position="static" className={classes.root}>
         <ToolBar>
-              <NavMenu />
+              {loggedIn ? <NavMenu />:null}
               <PrimarySearchBar />
         </ToolBar>
       </AppBar>
     )
   }
 
-export default SiteBar
+  const mapStateToProps = state => {
+    return {
+      loggedIn: state.auth.logg
+    }
+  }
+
+export default connect(mapStateToProps)(SiteBar)
