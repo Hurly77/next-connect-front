fbdc7169229d2d55cf643b981254df837fa9ecc8
cameron <cameron.leverett@yahoo.com>
2020-11-22 20:53:23 -0800

    changed feed page to functional component

--- src/Pages/FeedPage.js
+++ src/Pages/FeedPage.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
 import Posted from '../containers/Posted'
 import NewPost from '../components/Feed/NewPost'
 
-export default class FeedPage extends Component {
-  render() {
-    return (
-      <div>
-        <NewPost />
-        <Posted />
-      </div>
-    )
-  }
+const FeedPage = () => {
+  return (
+    <>
+      <NewPost />
+      <Posted />
+    </>
+  )
 }
+export default FeedPage
