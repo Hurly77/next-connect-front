85649284a1192b577d10245d0e2e849e31be60d5
cameron <cameron.leverett@yahoo.com>
2020-11-22 20:51:45 -0800

    code not needed for now

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { checkLoggedIn } from '../../Redux/actions/authActions';
-import { Link } from 'react-router-dom'
 
-const Home = ({loggedIn}) => {
-    if(!loggedIn){
-      return (
-        <div>
-          <h1>Welocome to, Next-connect</h1>
-          <Link to="/login">Login</Link>
-          <br />
-          <Link to="/signup">Signup</Link>
-        </div>
-      )
-    } else {
-     return( <>
-        <h1>Welcome</h1>
-      </>)
-    }
+const Home = () => {
+  return (
+    <>
+    </>
+  )
 }
 
 const mapStateToProps = state => {
