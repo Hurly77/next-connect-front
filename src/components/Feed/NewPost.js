fef4a8bb2c428fcb3fd80a0bde3ad83475d85ff0
cameron <cameron.leverett@yahoo.com>
2020-11-22 20:51:05 -0800

    mad a component for new post by user

--- src/components/Feed/NewPost.js
+++ src/components/Feed/NewPost.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react'
-
-export default class NewPost extends Component {
+import { connect } from 'react-redux'
+import { createPost } from '../../Redux/actions/postActions'
+ 
+class NewPost extends Component {
   state = {
-    text: ''
+    text: '',
+    img: null,
+    user_id: this.props.user
+  }
+
+  handleChange = e => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.createPost(this.state)
   }
 
   render() {
+    console.log(this.props)
     return (
-      <input></input>
+      <div className="new-post-card">
+        <form onSubmit={this.handleSubmit}>
+          <input className="text-for-new-post" type="text" placeholder="how are you feeling" onChange={this.handleChange} />
+          <button type="submit" className="btn-none"/>
+        </form>
+        <div>
+          <div className="multi-media-select">
+            <div className="mm">♂ photo/video</div>
+            <div className="mm">♫ music</div>
+            <div className="mm">♪ emoticon</div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
+
+const mapPropsToState = state => {
+  return {
+    user: state.auth.currentUser.id
+  }
+}
+
+export default connect(mapPropsToState, { createPost })(NewPost)
