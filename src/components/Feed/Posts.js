cda4a970de0389e0e2ffeb453d461f632a772a18
cameron <cameron.leverett@yahoo.com>
2020-11-23 17:44:04 -0800

    removed console log

--- src/components/Feed/Posts.js
+++ src/components/Feed/Posts.js
@@ -2,13 +2,11 @@ import React from 'react'
 import CommentCard from './CommentCard'
 
 const Posts = ({text}) => {
-  console.log(text, "posts")
     return (
       <div className="post-text"> 
           <p>{text}</p>
         
         <div></div>
-
           <CommentCard />
       </div>
     )
