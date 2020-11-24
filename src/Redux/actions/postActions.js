d00ca44e3a0ef3ff9c696c4f674904185da80722
cameron <cameron.leverett@yahoo.com>
2020-11-23 17:46:24 -0800

    added dispatch to post action

--- src/Redux/actions/postActions.js
+++ src/Redux/actions/postActions.js
@@ -1,23 +1,32 @@
 const apiUrl = 'http://localhost:3000/api/v1';
 
 export const createPost = (post) => {
-	fetch(`${apiUrl}/posts`, {
-		method      : 'POST',
-		headers     : {'Content-Type': 'application/json'},
-		credentials : 'include',
-		body        : JSON.stringify({post: post}),
-	});
+	return (dispatch) => {
+		fetch(`${apiUrl}/posts`, {
+			method      : 'POST',
+			headers     : {'Content-Type': 'application/json'},
+			credentials : 'include',
+			body        : JSON.stringify({post: post}),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				dispatch({
+					type    : 'NEW_POST',
+					payload : {
+						posts : data,
+					},
+				});
+			});
+	};
 };
 
 export const fetchPosts = () => {
 	return (dispatch) => {
-		fetch(`${apiUrl}/posts`).then((res) => res.json()).then(
-			(data) => {
-				dispatch({
-					type    : 'POSTS',
-					payload : {posts: data},
-				});
-			}
-		);
+		fetch(`${apiUrl}/posts`).then((res) => res.json()).then((data) => {
+			dispatch({
+				type    : 'POSTS',
+				payload : {posts: data},
+			});
+		});
 	};
 };
