294907a317444d8d491b291569ddaf37e58c057d
cameron <cameron.leverett@yahoo.com>
2020-11-23 17:45:06 -0800

    put login component into Home dir

---
+++ src/components/Home/Login.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {login} from '../../Redux/actions/authActions';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+
+class Login extends Component {
+	state = {
+		email    : '',
+		password : '',
+	};
+
+	handleOnChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	handleOnSubmit = (e) => {
+		e.preventDefault();
+		this.props.login(this.state, this.props.history);
+	};
+
+	render() {
+		return (
+			<div className="login-card">
+				<br />
+				<form onSubmit={this.handleOnSubmit}>
+					<input placeholder="email" name="email" type="text" value={this.state.email} onChange={this.handleOnChange} />
+					<br />
+					<input placeholder="password" type="password" name="password" value={this.state.password} onChange={this.handleOnChange} />
+					<br />
+					<button type="submit" className="login-btn">Login</button>
+				</form>
+				<br />
+				<div className="signup-btn-contain">
+				<button className="signup-btn">
+				<Link className="signup-link" to="/signup">Signup</Link>
+				</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default withRouter(connect(null, {login})(Login))
