3ccea75da16544e1aa0a7b02e99dccd1e0460d99
cameron <cameron.leverett@yahoo.com>
2020-11-24 18:20:46 -0800

    need signup button

---
+++ src/components/Home/SignupButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {useState} from 'react'
+import Modal from './Modal'
+
+export const SignupButton = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className="signup-btn-contain">
+    <button className="open-popup" onClick={() => setIsOpen(true)}>Create New Profile</button>
+    <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        form
+      </Modal>						
+  </div>
+  )
+}
+
+export default SignupButton
