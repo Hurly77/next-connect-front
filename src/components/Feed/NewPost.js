279a7b91356552e7f8447c5f324532881467cc27
cameron <cameron.leverett@yahoo.com>
2020-11-24 18:19:49 -0800

    misc

--- src/components/Feed/NewPost.js
+++ src/components/Feed/NewPost.js
@@ -1,49 +1,65 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import { createPost } from '../../Redux/actions/postActions'
- 
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {createPost} from '../../Redux/actions/postActions';
+
 class NewPost extends Component {
-  state = {
-    text: '',
-    img: null,
-    user_id: this.props.user
-  }
+	state = {
+		text    : '',
+		img     : null,
+		user_id : this.props.user,
+	};
 
-  handleChange = e => {
-    this.setState({
-      text: e.target.value
-    })
-  }
+	handleChange = (e) => {
+		this.setState({
+			...this.state,
+			text : e.target.value,
+		});
+	};
 
-  handleSubmit = e => {
-    e.preventDefault()
-    this.props.createPost(this.state)
-  }
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.createPost(this.state);
+		this.setState(
+			{
+				...this.state,
+				text    : '',
+				img     : null,
+				user_id : this.props.user,
+			},
+			() => console.log(this.state),
+		);
+	};
 
-  render() {
-    return (
-      <div className="new-post-card">
+	render() {
+		return (
+			<div className="new-post-card">
         <form onSubmit={this.handleSubmit}>
-          <input className="text-for-new-post" type="text" placeholder="how are you feeling" onChange={this.handleChange} />
-          <button type="submit" className="btn-none"/>
-        </form>
-        <div>
-          <div className="multi-media-select">
-            <div className="mm">♂ photo/video</div>
-            <div className="mm">♫ music</div>
-            <div className="mm">♪ emoticon</div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+
+          <input 
+          className="text-for-new-post" 
+          type="text" 
+          placeholder="how are you feeling" 
+          onChange={this.handleChange} 
+          value={this.state.text} />
+
+					<button type="submit" className="btn-none" />
+				</form>
+				<div>
+					<div className="multi-media-select">
+						<div className="mm">♂ photo/video</div>
+						<div className="mm">♫ music</div>
+						<div className="mm">♪ emoticon</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
-const mapPropsToState = state => {
-  return {
-    user: state.auth.currentUser.id
-  }
-}
+const mapPropsToState = (state) => {
+	return {
+		user : state.auth.currentUser.id,
+	};
+};
 
-export default withRouter(connect(mapPropsToState, { createPost })(NewPost))
+export default connect(mapPropsToState, {createPost})(NewPost);
