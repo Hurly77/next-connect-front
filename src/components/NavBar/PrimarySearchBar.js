87f724e00fc791fff71902f65b2d62e572418e85
cameron <cameron.leverett@yahoo.com>
2020-11-29 11:43:38 -0800

    added state to search

--- src/components/NavBar/PrimarySearchBar.js
+++ src/components/NavBar/PrimarySearchBar.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react'
 
 class PrimarySearchBar extends Component {
 
+  state = {
+    query: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      query: e.target.value
+    })
+  }
+  
   render() {
     return (
       <div>
-          <input type="text" name="search" placeholder="Search" className="search" />
+          <input type="text" name="search" placeholder="Search" className="search" onChange={this.handleChange} />
       </div>
     )
   }
