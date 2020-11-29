df1fa0c802ed0b56f2d8855b322f7032d27bde5a
cameron <cameron.leverett@yahoo.com>
2020-11-29 11:44:15 -0800

    added a mock up for testing/design

--- src/components/Profile/About.js
+++ src/components/Profile/About.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-export const About = () => {
+const About = ({user}) => {
+  console.log(user)
   return (
-    <div>
-      
+  <div className="about-wrapper">
+  <h2 className="about-title">About</h2>
+    <div className="about-details">
+      <ul>
+        <li>{user.work}</li>
+        <li>{user.education}</li>
+        <li>{user.relationship}</li>
+        <li>{user.lives}</li>
+        <li>{user.from}</li>
+        <li>{user.joined}</li>
+      </ul>
     </div>
+  </div>
   )
 }
+
+
+const mapStateToProps = state => {
+  return {
+    user: state.auth.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(About)
