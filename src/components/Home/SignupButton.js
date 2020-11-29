198631e26b79adcf0949d3078e2d31f012162255
cameron <cameron.leverett@yahoo.com>
2020-11-28 17:49:12 -0800

    changed name of Modal

--- src/components/Home/SignupButton.js
+++ src/components/Home/SignupButton.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {useState} from 'react'
-import Modal from './Modal'
+import SignupModal from './SignupModal'
 
 export const SignupButton = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <div className="signup-btn-contain">
     <button className="open-popup" onClick={() => setIsOpen(true)}>Create New Profile</button>
-      <Modal open={isOpen} onClose={() => setIsOpen(false)} />						
+      <SignupModal open={isOpen} onClose={() => setIsOpen(false)} />						
   </div>
   )
 }
