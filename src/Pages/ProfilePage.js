e8e3a655905c9dd5f9934c502e3b97aea842dadf
cameron <cameron.leverett@yahoo.com>
2020-11-29 11:46:07 -0800

    changed import name

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProfileHead from '../containers/ProfileHead';
 import {Link, useLocation, Switch, Route, useRouteMatch} from 'react-router-dom';
-import {Me} from '../components/Profile/About'
+import About from '../components/Profile/About'
 import Friends from '../components/Profile/Friends'
 import Posts from '../components/Profile/Posts'
 
@@ -36,7 +36,7 @@ const ProfilePage = () => {
 			</div>
 			<div>
 				<Switch>
-          <Route exact path={`${path}`} component={Me} />
+          <Route exact path={`${path}`} component={About} />
 					<Route path={`${path}/friends`} component={Friends} />
 					<Route path={`${path}/posts`} component={Posts} />
 				</Switch>
