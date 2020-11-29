483dfb6c37cd0b01d3bf47e30e62c69c530fa210
cameron <cameron.leverett@yahoo.com>
2020-11-28 17:52:42 -0800

    added head to page

---
+++ src/containers/ProfileHead.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile from '../components/Profile/Profile'
+import ProfileImage from '../components/Profile/ProfileImage'
+
+
+
+export default function ProfileHead() {
+  return (
+    <div className="profile-banner">
+      <Profile />
+      <ProfileImage />
+    </div>
+  )
+}
