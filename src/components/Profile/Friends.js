f811e1c09a05fb899d8a77c18ae6e0e938df6487
cameron <cameron.leverett@yahoo.com>
2020-11-28 17:50:01 -0800

    dummy text for testing

--- src/components/Profile/Friends.js
+++ src/components/Profile/Friends.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import ProfileHead from '../../containers/ProfileHead'
 
 export default class Friends extends Component {
   render() {
     return (
       <div>
-        
+        <h1>Friends</h1>
       </div>
     )
   }
