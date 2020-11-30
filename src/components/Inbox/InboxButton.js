9e3232e4f5601e71bbb6ba7b860acfdcc27c2a4d
cameron <cameron.leverett@yahoo.com>
2020-11-30 13:26:40 -0800

    added button Icon for Inbox

--- src/components/Inbox/InboxButton.js
+++ src/components/Inbox/InboxButton.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {useState} from 'react'
 import MessageModal from './MessageModal'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faInbox} from '@fortawesome/free-solid-svg-icons'
 
 export const InboxButton = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const inbox = <FontAwesomeIcon size="2x" icon={faInbox} />
   return (
     <div className="inbox-button">
     <button className="open-inbox" onClick={() => setIsOpen(true)}>
-      Messages
+      {inbox}
     </button>
       <MessageModal open={isOpen} onClose={() => setIsOpen(false)} />						
   </div>
