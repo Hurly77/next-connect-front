4ee0cba3eab507190962184dc0a33c4680f7c0cb
cameron <cameron.leverett@yahoo.com>
2020-11-30 13:28:33 -0800

    added Icon for Logout

--- src/components/NavBar/Logout.js
+++ src/components/NavBar/Logout.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 import { logout } from '../../Redux/actions/authActions'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
 
 class Logout extends Component {
   render () {
+    const logout = <FontAwesomeIcon size="2x" icon={faSignOutAlt} />
     if (this.props.loggedIn){
       return (
-        <div className="logout-div">
+        <>
          <Link to="#" onClick={() => this.props.logout(this.props.history)}>
-              <button type="button" className="btn-logout">Logout</button>
+              <button type="button" className="btn-logout">{logout}</button>
           </Link>
-        </div>
+        </>
       )
       }else{
        return( <>
