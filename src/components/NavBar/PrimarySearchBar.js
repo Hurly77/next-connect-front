21d7f5203d394428d72f87c3bf839484523b09a0
cameron <cameron.leverett@yahoo.com>
2020-11-30 13:29:16 -0800

    added some more functionality for search

--- src/components/NavBar/PrimarySearchBar.js
+++ src/components/NavBar/PrimarySearchBar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import query from '../../Redux/actions/queryActions'
+import {withRouter} from 'react-router-dom'
 
 class PrimarySearchBar extends Component {
 
@@ -12,14 +15,27 @@ class PrimarySearchBar extends Component {
       query: e.target.value
     })
   }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    query(this.state.query)
+    this.props.history.push('/results')
+    this.setState({
+      ...this.state,
+      query: ''
+    })
+  }
   
   render() {
     return (
       <div>
+        <form onSubmit={this.handleSubmit}>
           <input type="text" name="search" placeholder="Search" className="search" onChange={this.handleChange} />
+          <button type="submit">fdfd</button>
+        </form>
       </div>
     )
   }
 }
 
-export default PrimarySearchBar
+export default withRouter(connect(null, {query})(PrimarySearchBar))
