e71813be18d0235b17bb5344f48e24d6303446cf
cameron <cameron.leverett@yahoo.com>
2020-12-02 20:15:13 -0800

    removed some stuff

--- src/components/NavBar/Result.js
+++ src/components/NavBar/Result.js
@@ -1,48 +1,12 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {request, deny} from '../../Redux/actions/friendActions';
+import React, {Component} from 'react';
+import RequestButton from './RequestButton'
 
-const Result = ({user, pending, currentUser}) => {
-	if (!pending.includes(user)) {
-		return (
-			<div>
-				<h1>
-					{user.first_name} {user.last_name}
-				</h1>
-				<img className="search-avatar" src={user.avatar} alt="hello" />
-				<br />
-				<button onClick={request(currentUser.id, user.id)}>HI</button>
-				<p>
-					<span>Joined</span> : {user.joined}
-				</p>
-			</div>
-		);
-	} else {
-		return(<div>
-			<h1>
-				{user.first_name} {user.last_name}
-			</h1>
-			<img className="search-avatar" src={user.avatar} alt="hello" />
-			<br />
-			<button onClick={deny(currentUser, user)}>Nye</button>
-			<p>
-				<span>Joined</span> : {user.joined}
-			</p>
-		</div>)
+class Result extends Component {
+	render() {
+   return( 
+      <div>
+      <RequestButton key={this.props.user.id} />
+    </div>)
 	}
-};
-
-const mapStateToProps = (state) => {
-	return {
-		pending     : state.auth.pendingFriends,
-		currentUser : state.auth.currentUser,
-	};
-};
-
-mapDispatchToProps = dispatch => {
-  return {
-    request: () => dispatch({currentUser, user})
-  }
 }
-
-export default connect(mapStateToProps, mapStateToProps})(Result);
+export default Result;
