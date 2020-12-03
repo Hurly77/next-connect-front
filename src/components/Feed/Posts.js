baf9cb9b3532b6aeddf5b3ebce72e0647867b5dc
cameron <cameron.leverett@yahoo.com>
2020-12-02 20:13:04 -0800

    added more props to work with

--- src/components/Feed/Posts.js
+++ src/components/Feed/Posts.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import CommentCard from './CommentCard'
 
-const Posts = ({text}) => {
+const Posts = ({text, img, user}) => {
     return (
       <div className="post-text"> 
+          <h3>{user.first_name}</h3>
+          <img>{img}</img>
           <p>{text}</p>
         
         <div></div>
