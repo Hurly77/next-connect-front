c5e481ad0d2a1b569ef1d5048e531f9fdb018673
cameron <cameron.leverett@yahoo.com>
2020-12-02 20:18:23 -0800

    added then moved a ton of stuff

--- src/Redux/reducers/authReducer.js
+++ src/Redux/reducers/authReducer.js
@@ -1,21 +1,27 @@
-
-const authReducer = (state = { loggedIn: false, currentUser: {}}, action) => {
-  switch(action.type) {
-    case 'AUTH_SUCCESS':
-     return {
-       ...state,
-       loggedIn: action.payload.loggedIn,
-       currentUser: action.payload.currentUser
-     }
-     case 'LOGOUT':
-      return {
-        ...state,
-        loggedIn: false,
-        currentUser: {},
-      };
-    default:
-      return state;
-  }
-}
+const authReducer = (
+	state = {
+		loggedIn       : false,
+		currentUser    : {},
+	},
+	action,
+) => {
+	switch (action.type) {
+		case 'AUTH_SUCCESS':
+			return {
+				...state,
+				loggedIn    : action.payload.loggedIn,
+				currentUser : action.payload.currentUser,
+			};
+		case 'LOGOUT':
+			return {
+				...state,
+				loggedIn    : false,
+				currentUser : {},
+			};
+			
+		default:
+			return state;
+	}
+};
 
 export default authReducer;
