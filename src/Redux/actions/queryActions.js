0e6e16632f04164996165cf06a849af45e2712a9
cameron <cameron.leverett@yahoo.com>
2020-12-02 20:18:00 -0800

    changed how I query for users

--- src/Redux/actions/queryActions.js
+++ src/Redux/actions/queryActions.js
@@ -1,14 +1,14 @@
 const apiUrl = 'http://localhost:3000';
 
-const query = (query) => {
+const query = (query, history) => {
 	return (dispatch) => {
-		fetch(`${apiUrl}/api/v1/search?query=${query}`).then((r) => r.json()).then(
-			(data) =>
-				dispatch({
-					type    : 'SEARCH_BY_NAME',
-					payload : {results: data},
-				})
-		);
+		fetch(`${apiUrl}/api/v1/users?query=${query}`).then((r) => r.json()).then((data) => {
+			dispatch({
+				type    : 'SEARCH_BY_NAME',
+				payload : {results: data.tree},
+			});
+			history.push('/results');
+		});
 	};
 };
 
