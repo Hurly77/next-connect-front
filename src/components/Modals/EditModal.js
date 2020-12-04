0d224e68450d8f575bd17693cbcdb748704e423e
cameron <cameron.leverett@yahoo.com>
2020-12-03 21:21:08 -0800

    removed inline style

--- src/components/Modals/EditModal.js
+++ src/components/Modals/EditModal.js
@@ -1,36 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import About from '../Profile/About'
+import EditAbout from '../Profile/EditAbout'
 
-const MODAL_STYLES = {
-  postition: 'fixed',
-  transform: 'translate(0, -50%)',
-  borderRadius: '5px',
-  width: '400px',
-  height: '500px',
-  backgroundColor: '#23395B',
-  marginLeft: '75%',
-  paddingTop: '10px',
-  paddingLeft: '20px',
-  paddingRight: '20px',
-  paddingBottom:'40px',
-  zIndex: 'auto',
-}
-
-const BUTTON_STYLE = {
-  backgroundColor: 'transparent',
-  paddingBottom: '50px',
-  border: 'none',
-  width: '15px',
-  height: '15px'
-}
-
-export const EditModal = ({open, children, onClose}) => {
+export const editModal = ({open, children, onClose}) => {
   if (!open) return null
   return ReactDom.createPortal (
     <> 
-      <div className="about-modal" style={MODAL_STYLES}>
-        <button style={BUTTON_STYLE} onClick={()=> onClose()}>↓</button>
+      <div className="edit-modal">
+        <button className="edit-modal-btn" onClick={()=> onClose()}>X</button>
+        <EditAbout />
         {children}
       </div>
     </>,
@@ -38,4 +16,4 @@ export const EditModal = ({open, children, onClose}) => {
   )
 }
 
-export default EditModal
+export default editModal
