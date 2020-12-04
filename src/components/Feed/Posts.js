7c73c317f36307e758105f9d922c40d7de26f576
cameron <cameron.leverett@yahoo.com>
2020-12-03 21:19:40 -0800

    added more info for each post

--- src/components/Feed/Posts.js
+++ src/components/Feed/Posts.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import CommentCard from './CommentCard'
 
-const Posts = ({text, img, user}) => {
+const Posts = ({post}) => {
+
     return (
-      <div className="post-text"> 
-          <h3>{user.first_name}</h3>
-          <img>{img}</img>
-          <p>{text}</p>
-        
-        <div></div>
+      <div className="post-text">
+        <div className="posted-by">
+          <h1>{post.users_full_name}</h1>
+          <img src={!post.users_avatar ? "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png" : post.users_avatar} width="100px" height="100px"></img>
+          <p id="time-posted">Posted On : {post.posted_at}</p>
+          <p>{post.text}</p>
+        </div>
+          <img src={!post.img ? "https://incrediblethings.com/wp-content/uploads/2016/04/giant-chocolate-chip-cookie-topped-with-chocolate-chip-cookies-e1461279976684.jpg" : post.img}></img>
+        <div className="posted-text">
+           <p>Like |  comments</p>
+          --------------------------------
           <CommentCard />
+        </div>
       </div>
     )
 }
