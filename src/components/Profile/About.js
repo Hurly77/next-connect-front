2d355db982563aa3640468a123806ebdd3d032a4
cameron <cameron.leverett@yahoo.com>
2020-12-03 21:21:21 -0800

    just added line breaks

--- src/components/Profile/About.js
+++ src/components/Profile/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {EditButton} from '../ModalButtons/EditButton'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faBriefcase, 
@@ -23,15 +24,23 @@ const About = ({user}) => {
   return (
   <div className="about-wrapper">
   <h2 className="about-title">About</h2>
+  <br />
     <div className="about-details">
-        <div>{work} {user.work}</div>
+        <div>{work} {!user.work ? <h4>Tell Us about yourself</h4>:user.work}</div>
+        <br />
         <div>{education}{user.education}</div>
+        <br />
         <div>{relationship}{user.relationship}</div>
+        <br />
         <div>{lives}{user.lives}</div>
+        <br />
         <div>{from}{user.from}</div>
+        <br />
         <div>{joined}{user.joined}</div>
+        <br />
   <p>{user.c_id}</p>
     </div>
+    <EditButton />
   </div>
   )
 }
