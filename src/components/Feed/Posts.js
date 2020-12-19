38e4548f9a7de4595bc9c4bfac3c12b8663fca95
cameron <cameron.leverett@yahoo.com>
2020-12-18 16:20:45 -0800

    refactored code

--- src/components/Feed/Posts.js
+++ src/components/Feed/Posts.js
@@ -2,16 +2,16 @@ import React from 'react'
 import CommentCard from './CommentCard'
 
 const Posts = ({post}) => {
-
     return (
       <div className="post-text">
         <div className="posted-by">
           <h1>{post.users_full_name}</h1>
-          <img src={!post.users_avatar ? "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png" : post.users_avatar} width="100px" height="100px"></img>
+          {!post.users_avatar ? <img src={process.env.PUBLIC_URL + "no_avatar.png"} alt="1" width="100px" height="100px" /> 
+          : <img src={post.users_avatar} width="100px" height="100px" alt="1"/>}
           <p id="time-posted">Posted On : {post.posted_at}</p>
           <p>{post.text}</p>
         </div>
-          <img src={!post.img ? "https://incrediblethings.com/wp-content/uploads/2016/04/giant-chocolate-chip-cookie-topped-with-chocolate-chip-cookies-e1461279976684.jpg" : post.img}></img>
+          {!post.img ? <img src={process.env.PUBLIC_URL + "cookie.jpg"} alt="1" /> : <img src={post.img} alt="1" />}
         <div className="posted-text">
            <p>Like |  comments</p>
           --------------------------------
