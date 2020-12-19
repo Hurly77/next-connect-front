f61c4b5a974407a518ad76511b8a4cf1c75e4d24
cameron <cameron.leverett@yahoo.com>
2020-12-18 16:20:59 -0800

    misc

--- src/components/Feed/UploadImageButton.js
+++ src/components/Feed/UploadImageButton.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faImage} from '@fortawesome/free-solid-svg-icons'
 
+
 export default class UploadImageButton extends Component {
   state = {
     file: null
