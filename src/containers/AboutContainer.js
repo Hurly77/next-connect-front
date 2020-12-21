3c7be0bd998d80a73f1b43c8ea97fc79dcddd304
cameron <cameron.leverett@yahoo.com>
2020-12-20 20:33:30 -0800

    removed unused import

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import About from '../components/Profile/About';
-import EditButton from '../components/ModalButtons/EditButton'
 
 export default class AboutContainer extends Component {
 	render() {
