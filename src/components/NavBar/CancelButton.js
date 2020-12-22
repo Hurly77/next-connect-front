cbcd6b2d1cb9a073dfd2eb9cd03249ea5443b507
cameron <cameron.leverett@yahoo.com>
2020-12-21 18:27:47 -0800

    added props/clickEvent

--- src/components/NavBar/CancelButton.js
+++ src/components/NavBar/CancelButton.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const CancelButton = ({cancel, aU, pU}) => {
+const CancelButton = ({cancel, aU, pU, history}) => {
   return (
-      <button onClick={cancel(aU, pU)}>cancel Request</button>
+      <button onClick={() => cancel(aU, pU, history)}>cancel Request</button>
   )
 }
 export default CancelButton
