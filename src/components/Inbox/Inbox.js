44f1a1088df00a6c5869732455322df9f384af52
cameron <cameron.leverett@yahoo.com>
2020-12-26 15:51:14 -0800

    added accept_request actin creator

--- src/components/Inbox/Inbox.js
+++ src/components/Inbox/Inbox.js
@@ -1,40 +1,46 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import checkFriends from '../../Redux/actions/friendActions'
-
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import checkFriends from '../../Redux/actions/friendActions';
+import AcceptFriend from '../NavBar/AcceptFriend';
+import {accept_request, deny} from '../../Redux/actions/friendActions';
 
 class Inbox extends Component {
+	componentDidMount() {
+		checkFriends(this.props.currentUser.id);
+	}
 
-  componentDidMount(){
-    checkFriends(this.props.currentUser.id)
-  }
-
-  render() {
-    if(this.props.requests.length > 0){
-      return this.props.requests.map(r => {
-      return (
-        <div className="dialog">
-          <h1>{r.first_name}</h1>
-          helo
-          <button>accept</button>
-        </div>
-      )
-    })
-  }
-  else {
-   return (
-   <div>
-      <h5>no, messages or requests</h5>
-    </div>
-    )}
-  }
+	render() {
+		debugger;
+		if (this.props.requests.length > 0) {
+			return this.props.requests.map((r) => {
+				return (
+					<div className="search-card">
+						<h1>{r.first_name}</h1>
+						helo
+						<AcceptFriend
+							aU={this.props.currentUser.id}
+							pU={r.id}
+							accept={this.props.accept_request}
+							deny={this.props.deny}
+						/>
+					</div>
+				);
+			});
+		} else {
+			return (
+				<div>
+					<h5>no, messages or requests</h5>
+				</div>
+			);
+		}
+	}
 }
 
-const mapStateToProps = state => {
-  return {
-    requests: state.friends.requests,
-    currentUser: state.auth.currentUser
-  }
-}
+const mapStateToProps = (state) => {
+	return {
+		requests    : state.friends.requests,
+		currentUser : state.auth.currentUser,
+	};
+};
 
-export default connect(mapStateToProps, {checkFriends})(Inbox)
+export default connect(mapStateToProps, {checkFriends, accept_request, deny})(Inbox);
