bb7c1bfdfbe2cc379545a7b202037d35a27f0c95
cameron <cameron.leverett@yahoo.com>
2020-12-26 15:53:42 -0800

    connect  component to store and grab friends

--- src/components/Profile/Friends.js
+++ src/components/Profile/Friends.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Friends extends Component {
+class Friends extends Component {
   render() {
+    debugger
     return (
       <div className="friends-list-wrapper">
         <h1 className="friends-list-title">Friends</h1>
         <div className="friends-list">
-
+          {this.props.friends.map(friend => {return <h1>{friend.first_name}</h1>})}
         </div>
       </div>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    friends: state.friends.friends
+  }
+}
+
+export default connect(mapStateToProps)(Friends)
