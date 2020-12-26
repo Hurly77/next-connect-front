bb532a6112fb0723ff7883d765a164f934e5a541
cameron <cameron.leverett@yahoo.com>
2020-12-26 15:52:46 -0800

    connected to store

--- src/components/NavBar/Result.js
+++ src/components/NavBar/Result.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {checkFriends} from '../../Redux/actions/friendActions'
 
-const Result = ({user, button}) => {
-   return (
-      <div key={user.id} className="search-card">
+const Result = ({user, button, currentUser, checkFriends}) => {
+	return (
+		<div key={user.id} className="search-card">
 			{user.first_name}
 			<p>
 				<span>Joined</span>
 				{user.joined}
 			</p>
-         {button}
+        {button}
 			<img src={user.avatar} alt="1" className="search-avatar" />
 		</div>
 	);
 };
 
-export default Result;
+const mapStateToProps = state => {
+	return {
+		currentUser: state.auth.currentUser
+	}
+}
+
+export default connect(mapStateToProps, {checkFriends})(Result);
