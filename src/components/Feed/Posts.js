510e04e1ea8423caa1fc678dd119a5b541885fb4
cameron <cameron.leverett@yahoo.com>
2020-12-28 16:06:56 -0800

    added more relative alt description

--- src/components/Feed/Posts.js
+++ src/components/Feed/Posts.js
@@ -6,12 +6,12 @@ const Posts = ({post}) => {
       <div className="post-text">
         <div className="posted-by">
           <h1>{post.users_full_name}</h1>
-          {!post.users_avatar ? <img src={process.env.PUBLIC_URL + "no_avatar.png"} alt="1" width="100px" height="100px" /> 
-          : <img src={post.users_avatar} width="100px" height="100px" alt="1"/>}
+          {!post.users_avatar ? <img src={process.env.PUBLIC_URL + "no_avatar.png"} alt="avatar" width="100px" height="100px" /> 
+          : <img src={post.users_avatar} width="100px" height="100px" alt="profile pic"/>}
           <p id="time-posted">Posted On : {post.posted_at}</p>
           <p>{post.text}</p>
         </div>
-          {!post.img ? <img src={process.env.PUBLIC_URL + "cookie.jpg"} alt="1" /> : <img src={post.img} alt="1" />}
+          {!post.img ? <img src={process.env.PUBLIC_URL + "cookie.jpg"} alt="cookie" /> : <img src={post.img} alt="img post" />}
         <div className="posted-text">
            <p>Like |  comments</p>
           --------------------------------
