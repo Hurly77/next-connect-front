a466355ba916df3e15664e3917a66147a3652ae5
cameron <cameron.leverett@yahoo.com>
2020-12-30 18:14:44 -0800

    changing file struct

---
+++ src/components/Modals/MessageModal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Inbox from '../Inbox/Inbox'
+
+const BUTTON_STYLE = {
+  backgroundColor: 'transparent',
+  paddingBottom: '50px',
+  border: 'none',
+  width: '15px',
+  height: '15px'
+}
+
+export const MessageModal = ({open, children, onClose}) => {
+  if (!open) return null
+  return ReactDom.createPortal (
+    <> 
+      <div className="inbox-modal">
+        <button style={BUTTON_STYLE} onClick={()=> onClose()}>↓</button>
+          <h1>hello</h1>
+        <Inbox />
+        {children}
+      </div>
+    </>,
+    document.getElementById('portal')
+  )
+}
+
+export default MessageModal
