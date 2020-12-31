f2e933de329e92cb83cbc5af4a8a4813911a35fe
cameron <cameron.leverett@yahoo.com>
2020-12-31 15:56:52 -0800

    added moved moved profileImage.css to import

--- src/components/Profile/ProfileImage.js
+++ src/components/Profile/ProfileImage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './profileImage.css'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import {updateUser} from '../../Redux/actions/authActions'
