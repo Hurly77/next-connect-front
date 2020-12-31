2e29ed475f733d6f19df82517aef7f2b6f33e11b
cameron <cameron.leverett@yahoo.com>
2020-12-31 15:57:55 -0800

    added imports, added mapFriends class method

--- src/containers/FriendsContainer.js
+++ src/containers/FriendsContainer.js
@@ -1,21 +1,26 @@
 import React, {Component} from 'react';
+import './friendsContainer.css';
 import {connect} from 'react-redux';
-import {checkFriends} from '../Redux/actions/friendActions'
-import Friend from '../components/Profile/Friend'
+import {checkFriends} from '../Redux/actions/friendActions';
+import Friend from '../components/Friends/Friend';
 
 class FriendsContainer extends Component {
 	componentDidMount = () => {
 		this.props.checkFriends(this.props.currentUser.id);
 	};
 
-	render() {
-		return this.props.friends.map((friend) => {
-			return (
-					<div className="friends-container">
-            <Friend props={friend}/>
-					</div>
-			);
+	mapFriends = (friends) => {
+		return friends.map((friend) => {
+			return <Friend key={friend.id} props={friend} />;
 		});
+	};
+
+	render() {
+		return ( 
+			<div className="friends-container">
+				{this.mapFriends(this.props.friends)}
+			</div>
+		)
 	}
 }
 
