087b527501e9cdc5c91e91026e448e913769565d
cameron <cameron.leverett@yahoo.com>
2020-12-31 15:56:10 -0800

    added an handleClick event to toggle search

--- src/components/NavBar/PrimarySearchBar.js
+++ src/components/NavBar/PrimarySearchBar.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import query from '../../Redux/actions/queryActions'
 import {withRouter} from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons'
 
 class PrimarySearchBar extends Component {
 
   state = {
-    query: ''
+    query: '',
+    hidden: false
+  }
+
+  handleClick = (e) => {
+    console.log('clicked')
+    this.setState(prevState => {
+      return {
+        ...this.state,
+        hidden: !prevState.hidden
+      }
+    })
   }
 
   handleChange = (e) => {
@@ -25,7 +38,8 @@ class PrimarySearchBar extends Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" name="search" placeholder="Search" className="search" onChange={this.handleChange} value={this.state.query}/>
+          <FontAwesomeIcon icon={faSearch} onClick={this.handleClick}/>
+          <input type="text" name="search" placeholder="Search" className={this.state.hidden ? 'hidden' : 'search'} onChange={this.handleChange} value={this.state.query}/>
         </form>
       </div>
     )
