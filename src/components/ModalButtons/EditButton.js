80a08d8dfb41399da5e0aafc70b75a154265895a
cameron <cameron.leverett@yahoo.com>
2021-01-05 12:07:05 -0800

    add useEffect, exit modal when click out of modal

--- src/components/ModalButtons/EditButton.js
+++ src/components/ModalButtons/EditButton.js
@@ -1,20 +1,48 @@
-import React from 'react'
-import {useState} from 'react'
-import EditModal from '../Modals/EditModal'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEdit} from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import {useState, useRef, useEffect} from 'react';
+import EditModal from '../Modals/EditModal';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEdit} from '@fortawesome/free-solid-svg-icons';
+
+const useOnClickOutside = (ref, handler) => {
+	useEffect(
+    () => {
+      const listener = event => {
+        if (!ref.current || ref.current.contains(event.target)) {
+          return;
+        }
+        handler(event);
+      };
+
+      document.addEventListener('mousedown', listener);
+      document.addEventListener('touchstart', listener);
+
+      return () => {
+        document.removeEventListener('mousedown', listener);
+        document.removeEventListener('touchstart', listener);
+      };
+    },
+		[ref, handler],
+	);
+};
 
 export const EditButton = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const Edit = <FontAwesomeIcon size="2x" icon={faEdit} />
-  return (
-    <div className="edit-button">
-    <button className="open-edit-btn" onClick={() => setIsOpen(true)}>
-      {Edit}
-    </button>
-      <EditModal open={isOpen} onClose={() => setIsOpen(false)} />						
-  </div>
-  )
-}
+  const doc = document.querySelector('.edit-modal')
+  const myRef = useRef();
+	const [isOpen, setIsOpen] = useState(false);
+	const Edit = <FontAwesomeIcon size="2x" icon={faEdit} />;
+	useOnClickOutside(myRef, () => setIsOpen(false));
+	return (
+		<div className="edit-button">
+			{isOpen ? (
+				<EditModal myRef={myRef} open={isOpen} onClose={() => setIsOpen(false)} />
+			) : (
+				<button className="open-edit-btn" onClick={() => setIsOpen(true)}>
+					{Edit}
+				</button>
+			)}
+		</div>
+	);
+};
 
-export default EditButton
+export default EditButton;
