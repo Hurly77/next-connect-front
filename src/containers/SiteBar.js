af85461adca0f4e7771d634b30959663eda2083c
cameron <cameron.leverett@yahoo.com>
2021-01-05 12:16:09 -0800

    removed material-UI style and just used css

--- src/containers/SiteBar.js
+++ src/containers/SiteBar.js
@@ -1,24 +1,13 @@
 import React from 'react'
+import './SiteBar.css'
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/Toolbar'
 import NavMenu from '../components/NavBar/NavMenu'
 import PrimarySearchBar from '../components/NavBar/PrimarySearchBar'
-import { makeStyles } from '@material-ui/core/styles'
-
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-    background: '#161925',
-    position: 'fixed',
-    overFlow: 'hidden',
-    top: 0
-  }
-})
 
 const SiteBar = () => {
-    const classes = useStyles()
     return (
-      <AppBar className={classes.root} >
+      <AppBar className="app-bar" id="siteBar">
         <ToolBar id="nav-bar">
               <PrimarySearchBar />
               <NavMenu />
