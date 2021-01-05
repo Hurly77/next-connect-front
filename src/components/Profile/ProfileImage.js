feec2f009681aa2e8db4749b0a13db33b02a612d
cameron <cameron.leverett@yahoo.com>
2021-01-05 12:13:03 -0800

    working, on active storage, needs to store avatar

--- src/components/Profile/ProfileImage.js
+++ src/components/Profile/ProfileImage.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react'
 import './profileImage.css'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import {updateUser} from '../../Redux/actions/authActions'
+import {uploadAvatar} from '../../Redux/actions/authActions'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPencilAlt} from '@fortawesome/free-solid-svg-icons'
 
 class ProfileImage extends Component {
   
   fileSelectAvatar = (e) => {
-    let newInfo = {...this.props.currentUser}
-    newInfo.avatar = URL.createObjectURL(e.target.files[0])
+    e.preventDefault()
+    debugger
+      this.props.uploadAvatar(this.props.currentUser, e.target.files[0])    
   }
   
   render() {
+    const avatar = this.props.currentUser.avatar
     return (
       <>
       <div className="Avatar">
           <input type="file" onChange={this.fileSelectAvatar} id="actual-btn" hidden />
-          <img id="fileUpload" src={this.props.currentUser.avatar} alt='0' className="avatar-photo"/>
+          <img id="fileUpload" src={avatar ? avatar : "https://treepress.net/wp-content/plugins/treepress/public/imgs/no-avatar.png"} alt='0' className="avatar-photo"/>
           <label className="upload-avatar" htmlFor="actual-btn"><FontAwesomeIcon icon={faPencilAlt} size="2x" id="uploaded"/></label>
         </div>
       </>
@@ -32,4 +34,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default withRouter(connect(mapStateToProps, {updateUser})(ProfileImage))
+export default withRouter(connect(mapStateToProps, {uploadAvatar})(ProfileImage))
