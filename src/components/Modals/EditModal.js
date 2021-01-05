6896d7e526e1cb9a4da20bae791a64c386336ba8
cameron <cameron.leverett@yahoo.com>
2021-01-05 12:07:42 -0800

    pass down props to component

--- src/components/Modals/EditModal.js
+++ src/components/Modals/EditModal.js
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import EditAbout from './EditAbout'
 
-export const editModal = ({open, children, onClose}) => {
+export const editModal = ({open, children, onClose, myRef}) => {
   if (!open) return null
   return ReactDom.createPortal (
     <> 
-      <div className="edit-modal">
+      <div ref={myRef} className="edit-modal">
         <button className="edit-modal-btn" onClick={()=> onClose()}>X</button>
-        <EditAbout />
+        <EditAbout open={open} onClose={onClose}/>
         {children}
       </div>
     </>,
