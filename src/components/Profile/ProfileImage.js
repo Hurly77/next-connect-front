ed74f8ea09f2a56efff08b396a9d0774ea692213
cameron <cameron.leverett@yahoo.com>
2021-01-07 12:31:29 -0800

    added LCM to update, state, and event onChange

--- src/components/Profile/ProfileImage.js
+++ src/components/Profile/ProfileImage.js
@@ -2,24 +2,40 @@ import React, { Component } from 'react'
 import './profileImage.css'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import {uploadAvatar} from '../../Redux/actions/authActions'
+import {uploadAvatar, updateProps} from '../../Redux/actions/authActions'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPencilAlt} from '@fortawesome/free-solid-svg-icons'
+import isEqual from 'lodash.isequal'
 
 class ProfileImage extends Component {
-  
-  fileSelectAvatar = (e) => {
-    e.preventDefault()
-    debugger
-      this.props.uploadAvatar(this.props.currentUser, e.target.files[0])    
+  state = {
+    avatar: this.props.currentUser.photo_url
   }
-  
+
+
+  componentDidUpdate(prevProps){
+    if(!isEqual(this.props.currentUser, prevProps.currentUser)){
+      this.props.updateProps(this.props.currentUser.id)      
+    }
+  }
+
+
+  fileSelectAvatar = (e) => {
+    this.setState({
+      avatar: URL.createObjectURL(e.target.files[0])
+    })
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    this.props.uploadAvatar(this.props.currentUser, data)
+  }
+
   render() {
-    const avatar = this.props.currentUser.avatar
+    const avatar = this.state.avatar
     return (
       <>
       <div className="Avatar">
-          <input type="file" onChange={this.fileSelectAvatar} id="actual-btn" hidden />
+          <input type="file" name="newPhoto" accept="image/*" multiple={false} onChange={this.fileSelectAvatar} id="actual-btn" hidden />
           <img id="fileUpload" src={avatar ? avatar : "https://treepress.net/wp-content/plugins/treepress/public/imgs/no-avatar.png"} alt='0' className="avatar-photo"/>
           <label className="upload-avatar" htmlFor="actual-btn"><FontAwesomeIcon icon={faPencilAlt} size="2x" id="uploaded"/></label>
         </div>
@@ -34,4 +50,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default withRouter(connect(mapStateToProps, {uploadAvatar})(ProfileImage))
+export default withRouter(connect(mapStateToProps, {uploadAvatar, updateProps})(ProfileImage))
