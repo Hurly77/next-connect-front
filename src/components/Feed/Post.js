4a42d7a8ac2a33a935b2c635c7cb851f45bffe51
cameron <cameron.leverett@yahoo.com>
2021-01-08 19:12:54 -0800

    arrange ing post card to look more styled

--- src/components/Feed/Post.js
+++ src/components/Feed/Post.js
@@ -4,15 +4,17 @@ import CommentCard from './CommentCard'
 
 const Post = ({post}) => {
     return (
-      <div className="post-text">
+      <div className="post">
         <div className="posted-by">
-          <h1>{post.users_full_name}</h1>
           {!post.users_avatar ? <img src={process.env.PUBLIC_URL + "no_avatar.png"} alt="avatar" width="100px" height="100px" /> 
           : <img src={post.users_avatar} width="100px" height="100px" alt="profile pic"/>}
-          <p id="time-posted">Posted On : {post.posted_at}</p>
-          <p>{post.text}</p>
+          <h4>{post.users_full_name}</h4>
+          <p id="time-posted">{post.posted_at}</p>
         </div>
+          <p className="post-caption">{post.text}</p>
+        <div class="img-container">
           {!post.img ? <img src={process.env.PUBLIC_URL + "cookie.jpg"} alt="cookie" /> : <img src={post.img} alt="img post" />}
+        </div>
         <div className="posted-text">
            <p>Like |  comments</p>
           --------------------------------
