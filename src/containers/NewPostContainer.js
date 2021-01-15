03ca909eddffef2852e05d4b7a0a2c617617f796
cameron <cameron.leverett@yahoo.com>
2021-01-14 22:21:00 -0800

    added container for new post but don't need

--- src/containers/NewPostContainer.js
+++ src/containers/NewPostContainer.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 import NewPost from '../components/Feed/NewPost'
 import UploadImages from '../components/Feed/UploadImages'
 import {postImages, createPost} from '../Redux/actions/postActions'
 
 
 class NewPostContainer extends Component {
+  constructor(props) {
+		super(props);
+		this.initState = {
+      photos: [],
+      tmp: [],
+			post : {
+				text            : '',
+				user_id         : this.props.user.id,
+				users_full_name : this.props.user.first_name + ' ' + this.props.user.last_name,
+				users_avatar    : this.props.user.photo_url,
+      },
+		};
+		this.state = this.initState;
+  }
+  
+  submitPhotos = () => {
+
+  }
+  
+  submitPost = () => {
+    
+  }
+
+
   render() {
     return (
-      <div>
+      <div className="new-post-card">
+        <form>
         <NewPost props={this.props} />
         <UploadImages state={{photos: this.state.photos, tmp:this.state.tmp}} />
+        </form>
       </div>
     )
   }
