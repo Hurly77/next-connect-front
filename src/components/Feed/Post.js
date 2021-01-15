8c0ec483706a339c51a043dfc0dd48879f5fabd3
cameron <cameron.leverett@yahoo.com>
2021-01-14 22:17:47 -0800

    changed vars and added condtion for photos length

--- src/components/Feed/Post.js
+++ src/components/Feed/Post.js
@@ -2,15 +2,29 @@ import React from 'react'
 import './post.css'
 import CommentCard from './CommentCard'
 
-const mapImages = ({images}) => {
-  return images.map((image) => {
-    return  <img src={image} alt="img post" />
+const mapImages = (photos) => {
+  return photos.map((image) => {
+    return  (
+    <div className="img">
+      <img src={image} alt="img post" />
+    </div>
+    )
   })
 }
 
-const Post = ({post}) => {
+const image = (photos) => {
+ if(photos[0]){ return (
+    <div className="single-img">
+      <img src={photos[0]}/>
+    </div>
+  )}
+}
+
+const Post = ({post, photos}) => {
+  const single = "img-container-single"
+  const container = "img-container"
     return (
-      <div className="post">
+      <div key={post.id} className="post">
         <div className="posted-by">
           {!post.users_avatar ? <img src={process.env.PUBLIC_URL + "no_avatar.png"} alt="avatar" width="100px" height="100px" /> 
           : <img src={post.users_avatar} width="100px" height="100px" alt="profile pic"/>}
@@ -18,8 +32,8 @@ const Post = ({post}) => {
           <p id="time-posted">{post.posted_at}</p>
         </div>
           <p className="post-caption">{post.text}</p>
-        <div className="img-container">
-         
+        <div className={photos.length > 1 ? container : single}>
+          {photos.length > 1 ? mapImages(photos) : image(photos)}
         </div>
         <div className="posted-text">
           <br />
