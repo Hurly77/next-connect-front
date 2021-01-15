6c67e29fcac3fb1f793564ae4710b5bf8b557db3
cameron <cameron.leverett@yahoo.com>
2021-01-14 22:17:00 -0800

    updated component to user a tmp, and form data

--- src/components/Feed/NewPost.js
+++ src/components/Feed/NewPost.js
@@ -8,12 +8,14 @@ class NewPost extends Component {
 	constructor(props) {
 		super(props);
 		this.initState = {
-			post : {
+			photos : [],
+			post  : {
 				text            : '',
 				user_id         : this.props.user.id,
 				users_full_name : this.props.user.first_name + ' ' + this.props.user.last_name,
 				users_avatar    : this.props.user.photo_url,
 			},
+			tmp: []
 		};
 		this.state = this.initState;
 	}
@@ -27,11 +29,43 @@ class NewPost extends Component {
 		});
 	};
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		const post = this.state.post;
+	selectPhotos = (e) => {
+		const files = e.target.files;
+		const a = []
+		const b = []
+		for(const file of files){
+			a.push(URL.createObjectURL(file))
+			b.push(file)
+		}
+		console.log(b)
+		
+		this.setState({
+			...this.state,
+			photos : [...this.state.photos.concat(b)],
+			tmp   : [...this.state.tmp.concat(a)],
+		});
+	};
 
-		this.props.createPost(post);
+	toggelImg = () => {
+		const tmp = this.state.tmp;
+		if (tmp) {
+			return tmp.map(file => {
+				return <img src={file} alt="hello" className="new-post-img" />;
+			})
+		}
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		const data = new FormData();
+		const post = this.state.post;
+		const photos = this.state.photos
+		for(let i=0;i<photos.length;i++){
+				data.append("photos[]", photos[i], photos[i].name)
+		}
+		console.log(data)
+
+		this.props.createPost(post, data)
 		this.setState({
 			...this.initState,
 		});
@@ -52,6 +86,19 @@ class NewPost extends Component {
 					<button type="submit" className="btn-none" />
 				</form>
 				<div className="new-photos-container">{this.toggelImg()}</div>
+				<div>
+					<label className="new-photo" htmlFor="file-btn">
+						<FontAwesomeIcon size="2x" icon={faImage} id="new-photo-upload" />
+					</label>
+					<input
+						id="file-btn"
+						type="file"
+						accept="image/*"
+						multiple={true}
+						onChange={this.selectPhotos}
+						hidden
+					/>
+				</div>
 			</div>
 		);
 	}
