e0594fe7b8e972973b7fab177c4c176e7afbf398
cameron <cameron.leverett@yahoo.com>
2021-01-17 20:06:17 -0800

    added added action for create_comment

--- src/Redux/reducers/postReducer.js
+++ src/Redux/reducers/postReducer.js
@@ -1,4 +1,5 @@
 export const createPost = (state = {userPosts: [], friendsPosts: [], allPosts: [], userPhotos: []}, action) => {
+  let idx;
   switch(action.type){
     case 'NEW_POST':
       const newpost = action.payload
@@ -29,6 +30,12 @@ export const createPost = (state = {userPosts: [], friendsPosts: [], allPosts: [
       ...state,
       userPhotos: action.payload.photos
     }
+    case 'CREATE_COMMENT':
+      idx = state.allPosts.findIndex(post => post.post.id === action.payload.id);
+      return {
+        ...state,
+        allPosts: [...state.allPosts.slice(0, idx), action.payload, ...state.allPosts.slice(idx + 1)]
+      }
     
     default:
       return state;
