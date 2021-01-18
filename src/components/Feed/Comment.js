f22fd1fb4abe579d2e791a7a16488e8ca98dee1f
cameron <cameron.leverett@yahoo.com>
2021-01-17 20:02:43 -0800

    added jsx for style

--- src/components/Feed/Comment.js
+++ src/components/Feed/Comment.js
@@ -4,9 +4,12 @@ import React from 'react'
 const Comment = ({comment}) => {
   return (
     <div className="posted-comment-container">
-      <img src={comment.users_avatar} width="50px" height="50px" alt="avatar" />
-      <p>{comment.posted_at}</p>
-      <p>{comment.text}</p>
+      <img className="posted-comment-avatar" src={comment.users_avatar} alt="avatar" />
+      
+      <div className="posted-comment-comment">
+      <p className="posted-comment-name">{comment.users_full_name}</p>
+      <p className="posted-comment-text">{comment.text}</p>
+      </div>
 
     </div>
   )
