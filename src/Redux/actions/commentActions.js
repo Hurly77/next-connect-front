e2eaab1d6c5f0f0495761106ca1488f7de14b50f
cameron <cameron.leverett@yahoo.com>
2021-01-17 20:05:25 -0800

    added post requet for comments

--- src/Redux/actions/commentActions.js
+++ src/Redux/actions/commentActions.js
@@ -1,11 +1,23 @@
-import React from 'react'
+const apiUrl = 'http://localhost:3000/api/v1'
 
-export const comment = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export const comment = (comment) => {
+ return (dispatch) => {
+   fetch(`${apiUrl}/posts/${comment.post_id}/comments`, {
+    method      : 'POST',
+    headers     : {'Content-Type': 'application/json'},
+    credentials : 'include',
+    body        : JSON.stringify({comment: comment})
+   })
+   .then(r=> r.json())
+   .then(
+     (data) => {
+       dispatch({
+         type: 'CREATE_COMMENT',
+         payload: data
+       })
+     }
+   )
+ }
 }
 
 export default comment
